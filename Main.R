a38546f25b7b96fab02b46ec7971fb7ea03e6c71
Juan Pablo Carreon Hidalgo <jpch_26@outlook.com>
2021-02-02 20:31:51 -0600

    Main R script

---
+++ Main.R
@@ -0,0 +1,26 @@
+# Main script to run the analysis -----------------------------------------
+
+# Preferably, before you run this script restart your R session: 
+# Ctrl+Shift+F10 if you are using RStudio
+
+# Packages
+library(ggplot2) # To make graphs
+library(dplyr) # To make some data transformations
+
+# 1 Simulating the data
+source("analysis/data_simulation.R")
+
+# 2 Linear regression analysis
+source("analysis/linear_regression.R")
+
+# 3 Non linear regression analysis
+source("analysis/non_linear_regression.R")
+
+# 4 Compare models 
+source("analysis/compare_models.R")
+
+# 5 Source presentation document. You need 'rmarkdown' package
+rmarkdown::render("presentation_github.Rmd", output_file = "README")
+
+# 6 Session info
+capture.output(sessionInfo(), file = "Session_Info.txt")
